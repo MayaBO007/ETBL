4d827b49f55e1080db216ce713e689b24880ac14
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-01-01 15:51:23 +0200

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@ var staticCacheName = "pwa";
 self.addEventListener("install", function (e) {
 	e.waitUntil(
 		caches.open(staticCacheName).then(function (cache) {
-			return cache.addAll(['/carApp/index.html', '/carApp/instructions/instructions.html'].map(url => location.pathname.replace('/instsall/serviceworker.js', url)));
+			return cache.addAll(['/ET_BL/index.html', '/ET_BL/instructions/instructions.html'].map(url => location.pathname.replace('/instsall/serviceworker.js', url)));
 			// return cache.addAll(['/']);
 		})
 	);
