f43153b6747cc4b13a7bcc8128288e9cb65f1dce
MayaBO007 <77677870+MayaBO007@users.noreply.github.com>
2023-01-05 09:45:36 +0200

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -3,7 +3,7 @@ var staticCacheName = "pwa";
 self.addEventListener("install", function (e) {
 	e.waitUntil(
 		caches.open(staticCacheName).then(function (cache) {
-			return cache.addAll(['/ETRL/index.html'].map(url => location.pathname.replace('serviceworker.js', url)));
+			return cache.addAll(['/ETBL/index.html'].map(url => location.pathname.replace('serviceworker.js', url)));
 			// return cache.addAll(['/']);
 		})
 	);
