a3e569cf3578fe670311677f188ebadb17c36402
Maya <mayabaror@gmail.com>
2023-01-10 14:36:43 +0200

    breaks

--- days/dayTwo/gameInterval.js
+++ days/dayTwo/gameInterval.js
@@ -114,7 +114,7 @@ async function startTraining() {
                             };
 
 
-                            if (countingCars >= 280 && breaks <= 2) {
+                            if (countingCars >= 290 && breaks <= 2) {
                                 clearInterval(sessionInterval);
                                 reset_redCar();
                                 reset_blueCar();
